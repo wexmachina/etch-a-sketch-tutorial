c714384beeb037e265334f3ac781e1b73abb4a47
wexmachina <106637824+wexmachina@users.noreply.github.com>
2022-11-16 20:57:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+let color = "black";
+let click = false;
+
 document.addEventListener("DOMContentLoaded", function(){
-    createContainer(32);
+    createContainer(16);
+
+    document.querySelector("body").addEventListener("click", function(e){
+        if(e.target.tagName != "BUTTON") {
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if (click) {
+                draw.innerHTML = "Now you can draw"
+            }
+            else {
+                draw.innerHTML = "Now you cannot draw"
+            }
+        }
+    })
+
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createContainer(size);
+    })
     console.log("hi");
 })
 
@@ -7,4 +29,46 @@ function createContainer(size){
     let container = document.querySelector(".container");
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size*size;
+
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        div.addEventListener("mouseover", colorDiv)
+        container.insertAdjacentElement("beforeend", div);
+    }
+}
+
+function getSize() {
+    let input = prompt("What will be the size of the board?");
+    let message = document.querySelector("#message");
+    if (input == "") {
+        message.innerHTML = "Please provide a number";
+    }
+    else if (input < 0 || input > 100) {
+        message.innerHTML = "Provide a number between 1 and 100"
+    }
+    else {
+       message.innerHTML = "Now you can play!"
+       return input;
+    }
+}
+
+function colorDiv () {
+    if(click) {
+        if (color == "random") {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = "black";
+        }
+    }
+}
+
+function setColor(colorChoice) {
+    color = colorChoice;
+}
+
+function resetContainer() {
+    let divs = document.querySelectorAll("div");
+    divs.forEach((div) => div.style.backgroundColor = "white");
 }
